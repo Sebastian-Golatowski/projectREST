c101700437ee667eb3e038cb0d0c9407b7e60a7d
imtelligent9190 <s.golatowski5@gmail.com>
2025-05-17 19:22:12 +0200

    edit

--- Backend/routes/note.js
+++ Backend/routes/note.js
@@ -1,11 +1,19 @@
 import  express  from "express";
 import * as noteController from "../controllers/noteControler.js"
+import { tokenMiddleware } from "../backedLogic/tokenFunc.js";
 
 const router = express.Router();
 
 router
   .route('/')
-  .post(noteController.edit)
+  .put(tokenMiddleware, noteController.edit)
+  .all((req, res) => {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  });
+
+router
+  .route('/:bookId')
+  .get(tokenMiddleware, noteController.getNote)
   .all((req, res) => {
     return res.status(405).json({ message: 'Method Not Allowed' });
   });
