0b3fb37b607f7d194bdee7d15c7aa3a5bb2c1aff
imtelligent9190 <s.golatowski5@gmail.com>
2025-06-06 23:00:03 +0200

    added timeout

--- start.bat
+++ start.bat
@@ -1,6 +1,9 @@
 @echo off
+REM Start Backend
+start "Backend" cmd /k "cd backend && npm start"
+
+timeout 5
+
 REM Start Frontend
 start "Frontend" cmd /k "cd frontend && npm run dev"
 
-REM Start Backend
-start "Backend" cmd /k "cd backend && npm start"
